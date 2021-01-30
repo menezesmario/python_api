3b32cfaf6f5124af19c30b708d928222544cdb9e
menezesmario <mario082@gmail.com>
2021-01-30 10:00:58 -0300

    Error 405

--- api.py
+++ api.py
@@ -13,4 +13,9 @@ def home():
 def not_found(error):
     return render_template("error.html")
 
+@app.errorhandler(405)
+def not_found2(error):
+    return "O verbo usado não existe"
+
+
 app.run(port=8080, debug=True)
